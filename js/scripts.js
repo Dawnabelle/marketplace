eff7e12bd57cdc234b90682e7c15522cb3992074
Dawn Mott and William Chu <dawn@>
2018-03-21 14:39:54 -0700

    add javascript code to finish loading receipt data

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,16 @@ $(function(){
     var greens = $("input:radio[name=greens]:checked").val();
     var fruit = $("input:radio[name=fruit]:checked").val();
     var herb = $("input:radio[name=herb]:checked").val();
+    var delivery = $("#delivery").val();
 
     $(".fullName").text(fullName);
     $(".streetAddress").text(streetAddress);
     $(".cityStateZip").text(cityStateZip);
     $(".bagType").text(bagType);
+    $(".greens").text(greens);
+    $(".fruit").text(fruit);
+    $(".herb").text(herb);
+    $(".delivery").text(delivery);
 
     $("#output").show();
 
