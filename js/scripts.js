42d2d1d4fdf508a30602f6b41a45bcc573d12247
Dawn Mott and William Chu <william@>
2018-03-21 14:53:31 -0700

    add css styling, change javascript code to hide order form after submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(function(){
   $("#orderForm").submit(function() {
+// Converts user inputs into variables
     var fullName = $("input#fullName").val();
     var streetAddress = $("input#streetAddress").val();
     var cityStateZip = $("input#cityStateZip").val();
@@ -8,7 +9,7 @@ $(function(){
     var fruit = $("input:radio[name=fruit]:checked").val();
     var herb = $("input:radio[name=herb]:checked").val();
     var delivery = $("#delivery").val();
-
+// Takes input variables and places in output spans
     $(".fullName").text(fullName);
     $(".streetAddress").text(streetAddress);
     $(".cityStateZip").text(cityStateZip);
@@ -17,7 +18,8 @@ $(function(){
     $(".fruit").text(fruit);
     $(".herb").text(herb);
     $(".delivery").text(delivery);
-
+// Hide order form show receipt
+    $("#orderForm").hide();
     $("#output").show();
 
     event.preventDefault();
